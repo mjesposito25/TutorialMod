3604c1d9924491a0694fb4943755737e9ca8b43d
mjesposito25 <matt.joseph.esposito@gmail.com>
2022-05-23 22:04:19 -0400

    Dowsing Rod

--- src/main/java/com/ice/tutorialmod/item/ModItems.java
+++ src/main/java/com/ice/tutorialmod/item/ModItems.java
@@ -1,6 +1,7 @@
 package com.ice.tutorialmod.item;
 
 import com.ice.tutorialmod.TutorialMod;
+import com.ice.tutorialmod.item.custom.DowsingRodItem;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -15,6 +16,9 @@ public class ModItems {
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB)));
     public static final RegistryObject<Item> RAW_CITRINE = ITEMS.register("raw_citrine",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB)));
+    public static final RegistryObject<Item> DOWSING_ROD = ITEMS.register("dowsing_rod",
+            () -> new DowsingRodItem(new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB)
+                    .durability(16)));
 
 
     public static void register(IEventBus eventBus) {
