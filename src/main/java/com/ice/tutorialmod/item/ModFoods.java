d34594879ab70a721607b2600f88bf741f26eec5
mjesposito25 <matt.joseph.esposito@gmail.com>
2022-06-03 22:05:18 -0400

    Cucumber

---
+++ src/main/java/com/ice/tutorialmod/item/ModFoods.java
@@ -0,0 +1,8 @@
+package com.ice.tutorialmod.item;
+
+import net.minecraft.world.food.FoodProperties;
+
+public class ModFoods {
+    public static final FoodProperties CUCUMBER = (new FoodProperties.Builder()).fast().nutrition(2)
+            .saturationMod(0.2F).build();
+}
